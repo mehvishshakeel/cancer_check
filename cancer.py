5f79cddea3e1d6054edc805fd8e9b7ef8b293b97
mehvishshakeel <113215107+mehvishshakeel@users.noreply.github.com>
2022-12-30 03:18:01 -0700

    Input variables

--- cancer.py
+++ cancer.py
@@ -11,3 +11,14 @@ df.hist(bins=50, figsize=(15, 15))
 plt.tight_layout()
 plt.show()
 
+print ('please enter in the details of ypur tumor \n')
+radius = input('radius mean \n')
+texture = input('texture mean \n')
+perimeter = input('perimeter mean \n')
+area = input('area mean \n')
+smoothness = input('smoothness mean \n')
+compactness = input('compactness mean \n')
+concavity = input('concavity mean \n')
+concave_points = input('concave_points mean \n')
+
+
