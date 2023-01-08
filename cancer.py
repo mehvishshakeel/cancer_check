bf4c9da4cbe7b9560bd8db2b5ae383b40ecf5cca
“nimna <102314757+notnimna4761@users.noreply.github.com>
2023-01-08 12:22:57 -0700

    added checking logic

--- cancer.py
+++ cancer.py
@@ -9,9 +9,9 @@ print (df)
 
 df.hist(bins=50, figsize=(15, 15))
 plt.tight_layout()
-plt.show()
 
-print ('please enter in the details of ypur tumor \n')
+#plt.show()
+print ('please enter in the following details\n')
 
 
 radius = input ('radius mean:')
@@ -19,6 +19,7 @@ while True:
     if (float(radius) <28.1) and (float(radius) > 6.98 ):
         break
     else:
+        print("The entered value falls beyond the acceptable limits\n")
         radius = input ('Enter correct radius mean :')    
 
 
@@ -27,6 +28,7 @@ while True:
     if (float(texture) <39.3) and (float(texture) > 9.71 ) :
         break
     else:
+        print("The entered value falls beyond the acceptable limits\n")
         texture = input ('Enter correct texture mean :')
 
 
@@ -35,6 +37,7 @@ while True:
     if (float(perimeter) <189) and (float(perimeter) > 43.8 ) :
         break       
     else:
+        print("The entered value falls beyond the acceptable limits\n")
         perimeter = input ('Enter correct perimeter mean :')   
 
             
@@ -43,6 +46,7 @@ while True:
     if (float(area) <2501) and (float(area) > 144 ) :
         break
     else:
+        print("The entered value falls beyond the acceptable limits\n")
         area = input ('Enter correct area mean :')
                 
                 
@@ -51,6 +55,7 @@ while True:
     if (float(smoothness) <0.16) and (float(smoothness) > 0.05 ) :
         break
     else:
+        print("The entered value falls beyond the acceptable limits\n")
         smoothness = input ('Enter correct smoothness mean :')
 
 
@@ -59,6 +64,7 @@ while True:
     if (float(compactness) <0.35) and (float(compactness) > 0.02 ) :
         break
     else:
+        print("The entered value falls beyond the acceptable limits\n")
         compactness = input ('Enter correct compactness mean :')
 
                         
@@ -67,6 +73,7 @@ while True:
     if (float(concavity) <0.43) and (float(concavity) > 0 ) :
         break
     else:
+        print("The entered value falls beyond the acceptable limits\n")
         concavity = input ('Enter correct concavity mean :')
 
 
@@ -75,8 +82,67 @@ while True:
     if (float(concave_points) <0.2) and (float(concave_points) > 0 ) :   
         break
     else:
+        print("The entered value falls beyond the acceptable limits\n")
         concave_points = input ('Enter correct concave points mean :')
 
+
+
+
+avg_radius = df["radius_mean"].mean()
+avg_texture = df["texture_mean"].mean()
+avg_perimeter = df["perimeter_mean"].mean()
+avg_area = df["area_mean"].mean()
+avg_smoothness = df["smoothness_mean"].mean()
+avg_compactness = df["compactness_mean"].mean()
+avg_concavity = df["concavity_mean"].mean()
+avg_concave_points = df["concave points_mean"].mean()
+
+
+avg_radius_up_bound = avg_radius + (avg_radius*.4)
+avg_texture_up_bound = avg_texture 
+avg_perimeter_up_bound =
+avg_area_up_bound =
+avg_smoothness_up_bound =
+avg_compactness_up_bound =
+avg_concavity_up_bound =
+avg_concave_points_up_bound =
+
+avg_radius_low_bound = avg_radius + (avg_radius*.4)
+avg_texture_low_bound = avg_texture
+avg_perimeter_low_bound = 
+avg_area_low_bound = 
+avg_smoothness_low_bound = 
+avg_compactness_low_bound = 
+avg_concavity_low_bound = 
+avg_concave_points_low_bound = 
+
+
+
+'''
+print("avg_radius\t",avg_radius)
+print("avg_texture\t",avg_texture)
+print("avg_perimeter\t",avg_perimeter)
+print("avg_area\t",avg_area)
+print("avg_smoothness\t",avg_smoothness)
+print("avg_compactness\t",avg_compactness)
+print("avg_concavity\t",avg_concavity)
+print("avg_concave_points\t",avg_concave_points)
+'''
+
+
+
+percentage = 0
+
+step = 100/8
+
+if ((radius < avg_radius_up_bound) or (radius > avg_radius_low_bound)):
+    percentage += step
+
+if ((texture < avg_texture_up_bound) or (texture > avg_texture_low_bound)):
+    percentage += step
+
+
+print(" based on past data we are",percentage,"%", "sure that this cancer is Malignant")
 
 
     
